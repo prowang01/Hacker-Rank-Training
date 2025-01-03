f82e428a1bef7792a383f592a3df91c06a554d0a
prowang01 <prosper082003@outlook.com>
2025-01-03 12:38:31 +0100

    Hacker Rank Day 1

---
+++ Division.py
@@ -0,0 +1,10 @@
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+    
+print(a//b)
+print(a/b)
+
+
+# % gives the rest by the division
+# // is the floor division, if we have 3.33 we take 3.
